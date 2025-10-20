7100b0e91392d8b78889b2a044af00c7203725a9
Grahame Bowland <grahame@bowland.au>
2025-10-20 14:53:31 +0800

    move from poetry to uv

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["beacon.theol.au"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:5961/",
+      },
+      "/docs": {
+        target: "http://localhost:5961/",
+      },
+      "/openapi.json": {
+        target: "http://localhost:5961/",
+      },
+    },
+  },
+});
