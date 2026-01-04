11298b67a2d8f27b577c90ca4393b0afa4dacc78
Grahame Bowland <grahame@bowland.au>
2026-01-04 23:45:12 +0800

    fix ruff issue

--- beacon/api.py
+++ beacon/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
